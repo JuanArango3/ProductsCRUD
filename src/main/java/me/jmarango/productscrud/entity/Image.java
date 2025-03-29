af576eb4e53285e49360ace89175c725ad9764ef
Juan <jm.arango03@gmail.com>
2025-03-29 15:51:03 -0500

    add entity classes for Product, Image, and User with relationships

---
+++ src/main/java/me/jmarango/productscrud/entity/Image.java
@@ -0,0 +1,26 @@
+package me.jmarango.productscrud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "images")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
---
+++ src/main/java/me/jmarango/productscrud/entity/Product.java
@@ -0,0 +1,47 @@
+package me.jmarango.productscrud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image mainImage;
+
+    @OneToMany(mappedBy = "product")
+    private List<Image> additionalImages;
+
+    @Column(nullable = false)
+    private float price;
+
+    @CreatedDate
+    private Instant createdAt = Instant.now();
+}
